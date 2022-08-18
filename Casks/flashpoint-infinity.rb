09a51648e4d6f8304cf673eeb31302a1761d4473
nosamu <71368227+n0samu@users.noreply.github.com>
2022-08-18 01:13:03 -0500

    Update download URL

--- Casks/flashpoint-infinity.rb
+++ Casks/flashpoint-infinity.rb
@@ -2,7 +2,7 @@ cask "flashpoint-infinity" do
 	version "0.5"
 	sha256 "3648a788441244dcde4c6aaa94f09264460564dae927f0f2f838a699420398f5"
 
-	url "https://bluepload.unstable.life/flashpoint11infinitymac.7z"
+	url "https://bluemaxima.org/flashpoint/Flashpoint%2011%20Infinity%20Mac.7z"
 	name "Flashpoint Infinity"
 	desc "A playable webgame library and archive. The Infinity edition allows downloading each piece of media on demand."
 	homepage "https://bluemaxima.org/flashpoint/"
